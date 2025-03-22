4c2d5cc10e88a7c50dce6693a3add12de1226a68
d.uryupin <d.uryupin@stroytlm.ru>
2025-03-22 10:51:21 +0300

    Обработка событий (замечания)

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -11,15 +11,15 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var post = Post(
+        val post = Post(
             id = 1,
             author = "Нетология. Университет интернет-профессий будущего",
             content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
             published = "21 мая в 18:36",
             likedByMe = false,
             likedCount = 10,
-            sharedCount = 1095,
-            visibledCount = 10_110
+            sharedCount = 1995,
+            visibledCount = 10_100_110
         )
 
         with(binding){
--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -13,10 +13,10 @@ class Post (var id: Long,
 
     @SuppressLint("DefaultLocale")
     private fun getShortCount(count: Long): String = when {
-        count >= 10_000_000 -> String.format("%.0f", count.toFloat()/1_000_000) + "M"
-        count >= 1_100_000 -> String.format("%.1f", count.toFloat()/1_000_000) + "M"
-        count >= 10_000 -> String.format("%.0f", count.toFloat()/1_000) + "k"
-        count >= 1_100 -> String.format("%.1f", count.toFloat()/1_000) + "k"
+        count >= 10_000_000 -> String.format("%.0f", (count - count % 1_000_000).toFloat()/1_000_000) + "M"
+        count >= 1_000_000 -> String.format("%.1f", (count - count % 100_000).toFloat()/1_000_000) + "M"
+        count >= 10_000 -> String.format("%.0f", (count - count % 1000).toFloat()/1_000) + "k"
+        count >= 1_000 -> String.format("%.1f", (count - count % 100).toFloat()/1_000) + "k"
         else -> count.toString()}
 
     fun getLikedCountShort(): String = getShortCount(likedCount)
